b8ab60b97980498f417cdb59028849bd3dfdcae1
anibal-ladera <anibal-ladera>
2023-10-07 18:07:28 +0000

    commit2

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC <a href="https://colab.research.google.com/github/anibal-ladera/speedtest/blob/main/Untitled4.ipynb" target="_parent"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/></a>
+
+# COMMAND ----------
+
 import pandas as pd
 from google.cloud import bigquery
 import os
